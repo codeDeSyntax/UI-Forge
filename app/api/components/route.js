0c106749562d0c1a056026984f8319c389180cfd
codeDeSyntax <minwidth49@gmail.com>
2024-09-17 06:40:35 +0000

    fixing api caching

--- app/api/components/route.js
+++ app/api/components/route.js
@@ -1,15 +1,23 @@
 import clientPromise from "@/lib/mongoclient";
 import { NextResponse } from "next/server";
 
-export async function GET(req,res) {
-  res.setHeader('Cache-Control', 'no-store');
-
+export async function GET(req) {
   try {
     const client = await clientPromise;
     const db = client.db();
     const components = await db.collection("uiComponent").find({}).toArray();
-    return NextResponse.json(components);
+
+    // Set Cache-Control header to prevent caching
+    const headers = new Headers({
+      'Cache-Control': 'no-store'
+    });
+
+    // Return the response with the header
+    return NextResponse.json(components, { headers });
   } catch (error) {
-    return NextResponse.json({ message: "Error fetching components", error: error.message });
+    return NextResponse.json(
+      { message: "Error fetching components", error: error.message },
+      { status: 500 }
+    );
   }
 }
